51a492fb424a5ea8c0df257233642ed2e7742482
Armin <armin@webhippie.de>
2015-04-30 19:01:18 +0200

    rewrite

--- dist/angular-picklist.js
+++ dist/angular-picklist.js
@@ -8,109 +8,71 @@
 /* global angular */
 'use strict';
 
-function ListEntry(originalIndex, data) {
-  this.originalIndex = originalIndex;
-  this.data = data;
-}
-angular.module('apicklist', []).factory('_', function() {
-  return window._; // assumes underscore has already been loaded on the page
-}).directive('picklist', [
-  '_',
-  function(_) {
+angular.module('picklist', []).directive('picklist', [
+
+  function() {
     return {
       restrict: 'E',
       transclude: true,
       replace: true,
-      template: '<div class="container-fluid" ng-cloak> <div class="row"> <div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="leftFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple ng-multiple="true" ng-model="leftSelected" ng-options="r.data for r in leftListRows | filter:{data:leftFilter}" style="overflow: auto;" ng-style="listCss"></select> </div><div class="small-2 columns text-center"> <div> <button type="button" class="button tiny expand" ng-click="moveRightSelected()"> <i class="fa fa-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveRightAll()" ng-show="showAllButtons"> <i class="fa fa-fast-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftSelected()"> <i class="fa fa-backward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftAll()" ng-show="showAllButtons"> <i class="fa fa-fast-backward fa-fw"></i> </button> </div></div><div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="rightFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple="multiple" ng-model="rightSelected" ng-options="r.data for r in rightListRows | filter:{data:rightFilter}" style="overflow: auto;" ng-style="listCss"></select> </div></div></div>',
+      template: '<div class="container-fluid" ng-cloak> <div class="row"> <div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="leftFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple ng-multiple="true" ng-model="leftSelected" ng-options="transformText(r) for r in leftListRowsModel | filter:leftFilter" style="overflow: auto;" ng-style="listCss"></select> </div><div class="small-2 columns text-center"> <div> <button type="button" class="button tiny expand" ng-click="moveRightSelected()"> <i class="fa fa-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveRightAll()" ng-show="showAllButtons"> <i class="fa fa-fast-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftSelected()"> <i class="fa fa-backward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftAll()" ng-show="showAllButtons"> <i class="fa fa-fast-backward fa-fw"></i> </button> </div></div><div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="rightFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple="multiple" ng-model="rightSelected" ng-options="transformText(r) for r in rightListRowsModel | filter:rightFilter" style="overflow: auto;" ng-style="listCss"></select> </div></div></div>',
       scope: {
         leftListRowsModel: '=leftListRows',
         rightListRowsModel: '=rightListRows',
-        listWidth: '@listWidth',
-        listHeight: '@listHeight',
-        showMoveAllButtons: '@'
+        showMoveAllButtons: '@',
+        displayText: '=display'
       },
+
       link: function(scope) {
-        function initializeRowLists() {
-          scope.leftListRows = _.map(scope.leftListRowsModel, function(element, index) {
-            return new ListEntry(index, element);
-          });
-          scope.rightListRows = _.map(scope.rightListRowsModel, function(element, index) {
-            return new ListEntry(index, element);
-          });
-        }
-        scope.listCss = {};
         scope.showAllButtons = scope.showMoveAllButtons || true;
-        if (scope.listWidth) {
-          scope.listCss['min-width'] = scope.listWidth + 'px';
-        }
-        if (scope.listHeight) {
-          scope.listCss.height = scope.listHeight + 'px';
-        }
-        initializeRowLists();
-        //indices of selected rows
+
         scope.leftSelected = [];
         scope.rightSelected = [];
         scope.leftFilter = '';
         scope.rightFilter = '';
-        /**
-         * moves only selected rows from left to right
-         */
+
+        scope.transformText = function(element) {
+          if (scope.displayText)
+            return scope.displayText(element);
+          else
+            return element;
+        };
+
         scope.moveRightSelected = function() {
-          //convert selected rows into raw data
-          var selectedData = scope.leftSelected.map(function(row) {
-            return row.data;
+
+          scope.rightListRowsModel = scope.rightListRowsModel.concat(scope.leftSelected);
+
+          scope.leftListRowsModel = _.reject(scope.leftListRowsModel, function(e) {
+            return _.indexOf(scope.leftSelected, e) != -1
           });
-          //add data to the right list
-          scope.rightListRowsModel = scope.rightListRowsModel.concat(selectedData);
-          //remove from left list
-          scope.leftSelected.forEach(function(element) {
-            scope.leftListRowsModel.splice(element.originalIndex, 1);
-          });
-          //reinitialize row models
-          initializeRowLists();
-          //clear selected lists
+
           scope.rightSelected = [];
           scope.leftSelected = [];
         };
-        /**
-         * moves only selected rows from right to left
-         */
+
         scope.moveLeftSelected = function() {
-          //convert selected rows into raw data
-          var selectedData = scope.rightSelected.map(function(row) {
-            return row.data;
+
+          scope.leftListRowsModel = scope.leftListRowsModel.concat(scope.rightSelected);
+          scope.rightListRowsModel = _.reject(scope.rightListRowsModel, function(e) {
+            return _.indexOf(scope.rightSelected, e) != -1
           });
-          //add data to the left list
-          scope.leftListRowsModel = scope.leftListRowsModel.concat(selectedData);
-          //remove from right list
-          scope.rightSelected.forEach(function(element) {
-            scope.rightListRowsModel.splice(element.originalIndex, 1);
-          });
-          //reinitialize row models
-          initializeRowLists();
-          //clear selected lists
+
           scope.rightSelected = [];
           scope.leftSelected = [];
         };
+
         scope.moveRightAll = function() {
-          //add data to the right list
           scope.rightListRowsModel = scope.rightListRowsModel.concat(scope.leftListRowsModel);
-          //remove data from left list
           scope.leftListRowsModel = [];
-          //reinitialize row models
-          initializeRowLists();
-          //clear selected lists
+
           scope.rightSelected = [];
           scope.leftSelected = [];
         };
+
         scope.moveLeftAll = function() {
-          //add data to the right list
           scope.leftListRowsModel = scope.leftListRowsModel.concat(scope.rightListRowsModel);
-          //remove data from left list
           scope.rightListRowsModel = [];
-          //reinitialize row models
-          initializeRowLists();
-          //clear selected lists
+
           scope.rightSelected = [];
           scope.leftSelected = [];
         };
@@ -122,6 +84,6 @@ angular.module('template/picklist.html', []).run([
   '$templateCache',
   function($templateCache) {
     'use strict';
-    $templateCache.put('template/picklist.html', '<div class="container-fluid" ng-cloak> <div class="row"> <div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="leftFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple ng-multiple="true" ng-model="leftSelected" ng-options="r.data for r in leftListRows | filter:{data:leftFilter}" style="overflow: auto;" ng-style="listCss"></select> </div><div class="small-2 columns text-center"> <div> <button type="button" class="button tiny expand" ng-click="moveRightSelected()"> <i class="fa fa-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveRightAll()" ng-show="showAllButtons"> <i class="fa fa-fast-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftSelected()"> <i class="fa fa-backward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftAll()" ng-show="showAllButtons"> <i class="fa fa-fast-backward fa-fw"></i> </button> </div></div><div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="rightFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple="multiple" ng-model="rightSelected" ng-options="r.data for r in rightListRows | filter:{data:rightFilter}" style="overflow: auto;" ng-style="listCss"></select> </div></div></div>');
+    $templateCache.put('template/picklist.html', '<div class="container-fluid" ng-cloak> <div class="row"> <div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="leftFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple ng-multiple="true" ng-model="leftSelected" ng-options="transformText(r) for r in leftListRowsModel | filter:leftFilter" style="overflow: auto;" ng-style="listCss"></select> </div><div class="small-2 columns text-center"> <div> <button type="button" class="button tiny expand" ng-click="moveRightSelected()"> <i class="fa fa-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveRightAll()" ng-show="showAllButtons"> <i class="fa fa-fast-forward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftSelected()"> <i class="fa fa-backward fa-fw"></i> </button> </div><div class="text-center"> <button type="button" class="button tiny expand" ng-click="moveLeftAll()" ng-show="showAllButtons"> <i class="fa fa-fast-backward fa-fw"></i> </button> </div></div><div class="small-5 columns"> <input placeholder="Search" type="text" class="form-control" ng-model="rightFilter" style="width: 100%; margin-bottom: 10px;"/> <select multiple="multiple" ng-model="rightSelected" ng-options="transformText(r) for r in rightListRowsModel | filter:rightFilter" style="overflow: auto;" ng-style="listCss"></select> </div></div></div>');
   }
 ]);
